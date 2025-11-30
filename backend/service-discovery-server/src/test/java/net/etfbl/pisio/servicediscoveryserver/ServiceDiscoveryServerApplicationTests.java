9c4efcfa3c89657f7d15ec9f90ea2923fd77b37f
vanjap1 <vpejic001@gmail.com>
2025-11-30 21:26:28 +0100

    feature: implement service discovery

---
+++ backend/service-discovery-server/src/test/java/net/etfbl/pisio/servicediscoveryserver/ServiceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.etfbl.pisio.servicediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
